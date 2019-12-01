bf2c01859cbac5417fd935e7b5618ff72c1b24d4
franek <franeklubi@gmail.com>
2019-12-01 19:53:50 +0100

    Add Police animation

--- animserv/animations.go
+++ animserv/animations.go
@@ -124,6 +124,63 @@ func GradientOverTimeFromJSON(a AnimationJSON) ([]ledgend.Animation, error) {
 }
 
 
+func PoliceFromJSON(a AnimationJSON) ([]ledgend.Animation, error) {
+    if ( len(a.Colors) < 2 ) {
+        return nil, errors.New("Not enough colors in police call")
+    }
+
+    var anims []ledgend.Animation
+
+    start, durations, cols := getParams(a)
+
+    white := animations.Sweep(
+        true, 0, 1,
+        ledgend.Color{255, 255, 255}, ledgend.Color{255, 255, 255},
+        time.Millisecond, start,
+    )
+
+    duration_half := durations[0]/2
+
+    gradient_l := animations.GradientOverTime(
+        true, 0, 0.4,
+        cols[0], cols[0],
+        ledgend.Color{0, 0, 0}, ledgend.Color{0, 0, 0},
+        duration_half, durations[1],
+        start,
+    )
+    gradient_l_back := animations.GradientOverTime(
+        true, 0, 0.4,
+        ledgend.Color{0, 0, 0}, ledgend.Color{0, 0, 0},
+        cols[0], cols[0],
+        duration_half, durations[1],
+        start.Add(duration_half),
+    )
+
+    gradient_r := animations.GradientOverTime(
+        true, 0.6, 1,
+        ledgend.Color{0, 0, 0}, ledgend.Color{0, 0, 0},
+        cols[1], cols[1],
+        duration_half, durations[1],
+        start,
+    )
+    gradient_r_back := animations.GradientOverTime(
+        true, 0.6, 1,
+        cols[1], cols[1],
+        ledgend.Color{0, 0, 0}, ledgend.Color{0, 0, 0},
+        duration_half, durations[1],
+        start.Add(duration_half),
+    )
+
+    anims = append(anims, white)
+    anims = append(anims, gradient_l...)
+    anims = append(anims, gradient_l_back...)
+    anims = append(anims, gradient_r...)
+    anims = append(anims, gradient_r_back...)
+
+    return anims, nil
+}
+
+
 func resolveAnimation(a AnimationJSON) ([]ledgend.Animation, error) {
     var (
         anims   []ledgend.Animation
@@ -159,6 +216,12 @@ func resolveAnimation(a AnimationJSON) ([]ledgend.Animation, error) {
 
         case "gradient":
             anims, err = GradientOverTimeFromJSON(a)
+            if ( err != nil ) {
+                return nil, err
+            }
+
+        case "police":
+            anims, err = PoliceFromJSON(a)
             if ( err != nil ) {
                 return nil, err
             }
