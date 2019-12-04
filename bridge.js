988d131e66886443425aa72cb7ed76ed4572b3da
Franek Lubi <franeklubi@gmail.com>
2019-12-04 15:00:45 +0100

    Add node-go animation 'bridge'

---
+++ bridge.js
@@ -0,0 +1,32 @@
+const generators = require('./generators.js')
+const request = require('request')
+
+
+function sendJSON(
+    JSON,                   // use generators.genSendJSON
+    port = 10108,           // optional arg
+    address = 'localhost'   // optional arg
+) {
+    return new Promise((res, rej) => {
+        request.post(
+            address+':'+port+'/json',
+            { json: JSON },
+            (err, r, b) => {
+                if (!err && (r == undefined || r.statusCode == 200)) {
+                    res(b)
+                } else {
+                    rej({
+                        error: err,
+                        response: r
+                    })
+                }
+            }
+        )
+    })
+}
+
+
+
+module.exports = {
+    sendJSON: sendJSON
+}
